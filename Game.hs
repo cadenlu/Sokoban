54c834dcefaeaef615bc71af1cfaf72f434e3d60
caden <z4h0n@ugrad.cs.ubc.ca>
2023-02-24 23:39:32 -0800

    fixed types in funcs act should be done

--- Game.hs
+++ Game.hs
@@ -86,9 +86,9 @@ act :: Action -> GameState -> Maybe GameState
 act (Move dir) state =
   let player = playerLocation state             -- gets playerLocation
       boxes = boxLocations state                -- gets boxLocations
-      newPos = moveInDirection player dir       -- gets new position after moving
+      newPos = moveInDirection player (Move dir)-- gets new position after moving
       newTile = tileAt (levelMap state) newPos  -- gets tile at the new position
-      (boxMoved, newBoxes) = moveBoxIfPossible boxes newPos dir -- TODO (function that moves boxes if possible)
+      (boxMoved, newBoxes) = moveBoxIfPossible state boxes newPos (Move dir) -- TODO (function that moves boxes if possible)
   in case newTile of
     Wall -> Just state
     _ -> -- either Empty or Storage
@@ -98,19 +98,33 @@ act (Move dir) state =
 act Quit _ = Nothing
 
 
--- moves box if box movement is possible
-  -- the function takes in our list of boxLocations and our newPos and dir (from act) and
-  -- returns if our box was moved, and our updated list of boxLocations
-moveBoxIfPossible :: [Coord] -> Coord -> Direction -> (Bool, [Coord]) -- TODO
-  -- check if newPos 'elem' boxLocations
+-- moves box if the tile after the box is not a wall or another box, returns if our boxMoved, and our newBoxes locations
+moveBoxIfPossible :: GameState -> [Coord] -> Coord -> Action -> (Bool, [Coord])
+moveBoxIfPossible state boxes boxPos (Move dir)
+  | not (boxPos `elem` boxes) = (False, boxes)  
+  | tileNext == Wall || boxPosNext `elem` boxes = (False, boxes)
+  | otherwise = (True, newBoxes)
+    where
+      tileNext = tileAt (levelMap state) boxPosNext
+      boxPosNext = moveInDirection boxPos (Move dir)
+      newBoxPos = moveInDirection boxPosNext (Move dir)
+      newBoxes = replace boxes boxPos newBoxPos
+
+
+-- helper for moveBoxIfPossible (replaces boxPos with newBoxPos)
+replace :: Eq a => [a] -> a -> a -> [a]
+replace [] _ _ = []
+replace (x:xs) old new
+  | x == old = new:replace xs old new
+  | otherwise = x:replace xs old new
 
 
 -- returns new coordinate after movement action (helper for act function)
-moveInDirection :: Coord -> Move -> Coord
-moveInDirection (x,y) MUp    = (x, y-1)
-moveInDirection (x,y) MDown  = (x, y+1)
-moveInDirection (x,y) MLeft  = (x-1, y)
-moveInDirection (x,y) MRight = (x+1, y)
+moveInDirection :: Coord -> Action -> Coord
+moveInDirection (x,y) (Move MUp)    = (x, y-1)
+moveInDirection (x,y) (Move MDown)  = (x, y+1)
+moveInDirection (x,y) (Move MLeft)  = (x-1, y)
+moveInDirection (x,y) (Move MRight) = (x+1, y)
 
 
 won :: GameState -> Bool
