186bdf0173bf0f46142009c8738b5e641a783ab4
verticallity <verticallity.name@gmail.com>
2023-02-27 13:32:57 -0800

    load levels from files

--- Main.hs
+++ Main.hs
@@ -1,34 +1,30 @@
 module Main where
 
+import Control.Monad
+import System.Environment
+
 import Game
+import Load
 import Terminal
 
 
--- TODO read from a file
-testLevelTiles :: [[Tile]]
-testLevelTiles =
-  [ [ Wall, Wall,  Wall,  Wall,    Wall ]
-  , [ Wall, Empty, Empty, Empty,   Wall ]
-  , [ Wall, Empty, Empty, Storage, Wall ]
-  , [ Wall, Empty, Empty, Empty,   Wall ]
-  , [ Wall, Wall,  Wall,  Wall,    Wall ]
-  ]
-testLevel :: GameState
-testLevel = GameState
-  { playerLocation = (1, 2)
-  , boxLocations = [(2, 2)]
-  , levelMap = mapFromDisplayOrderedTiles testLevelTiles
-  }
+loadLevelFromFile :: FilePath -> IO GameState
+loadLevelFromFile path = do
+  contents <- readFile path
+  let (warnings, level) = loadLevelFromString contents
+  foldl (>>) (return ()) $ map (\msg -> putStrLn ("warning: " ++ msg)) warnings
+  return level
 
 main :: IO ()
 main = do
+  levelPaths <- getArgs
   interface <- setupTerminalInterface
-  gameLoop interface testLevel
+  foldl (>>) (return ()) $ map (loadLevelFromFile >=> gameLoop interface) levelPaths
 
 gameLoop :: GameInterface i => i -> GameState -> IO ()
 gameLoop interface state = do
   render interface state
-  if won state then return ()
+  if won state then putStrLn "level completed!"
   else do
     action <- getInput interface
     let newState = act action state
