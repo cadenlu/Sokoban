2488bb75033ad2e982f40272031cc649e98b086c
verticallity <verticallity.name@gmail.com>
2023-02-27 23:31:16 -0800

    simple caching level solver

--- Main.hs
+++ Main.hs
@@ -5,15 +5,55 @@ import System.Environment
 
 import Game
 import Load
+import Solve
 import Terminal
 
 
+-- a command line argument
+data Arg = Option String | Level FilePath
+-- determine whether a command line argument is an option or a file
+parseArg :: String -> Arg
+parseArg ('-' : '-' : option) = Option option
+parseArg path = Level path
+
+-- filter array to Just entries of a function into Maybe
+filterMap :: (a -> Maybe b) -> [a] -> [b]
+filterMap f [] = []
+filterMap f (x:xs) = case f x of
+  Just y -> y : filterMap f xs
+  Nothing -> filterMap f xs
+
+-- takes paths to levels as command line arguments
+-- and presents them to the user to play, one after another
 main :: IO ()
 main = do
-  levelPaths <- getArgs
-  interface <- setupTerminalInterface
-  foldl (>>) (return ()) $ map (loadLevelFromFile >=> gameLoop interface) levelPaths
+  argsStrs <- getArgs
+  let args = map parseArg argsStrs
+  let levelPaths = filterMap (\a -> case a of Level path -> Just path ; _ -> Nothing) args
+  let options = filterMap (\a -> case a of Option opt -> Just opt ; _ -> Nothing) args
 
+  if "solve" `elem` options
+  then
+    foldl (>>) (return ()) $ map showSolution levelPaths
+  else do
+    interface <- setupTerminalInterface
+    foldl (>>) (return ()) $ map (loadLevelFromFile >=> gameLoop interface) levelPaths
+
+-- find and display a solution for a level
+showSolution :: FilePath -> IO ()
+showSolution path = do
+  state <- loadLevelFromFile path
+  let solnRev = solveRev state
+  putStrLn $ path ++ ": " ++ foldl (\str dir -> directionArrow dir : ' ' : str) [] solnRev
+  where
+    -- directional arrow from move direction, for displaying level solutions
+    directionArrow :: MoveDirection -> Char
+    directionArrow MUp = '↑'
+    directionArrow MDown = '↓'
+    directionArrow MLeft = '←'
+    directionArrow MRight = '→'
+
+-- the main game loop, playing a single level
 gameLoop :: GameInterface i => i -> GameState -> IO ()
 gameLoop interface state = do
   render interface state
---
+++ Solve.hs
@@ -0,0 +1,63 @@
+module Solve ( solve, solveRev ) where
+
+import Game
+
+
+-- the dynamic part of the game state, only concerning player and box locations
+-- for caching, box locations should be sorted
+type DynamicState = (Coord, [Coord])
+-- a state and directions to reach it
+type ReachedState = (DynamicState, [MoveDirection])
+
+-- shortest solution to a level, if found
+solve :: GameState -> [MoveDirection]
+solve = reverse . solveRev
+
+-- shortest solution to a level, if found, with the first element being the last move
+-- faster than solve if viable
+solveRev :: GameState -> [MoveDirection]
+solveRev startingState = solve' [] [(dynStateOf startingState, [])]
+  where
+    -- the dynamic parts of a given game state: player location and box locations
+    dynStateOf :: GameState -> DynamicState
+    dynStateOf state = (playerLocation state, boxLocations state)
+
+    -- states reachable from a given state. may contain duplicates
+    nextStates :: ReachedState -> [ReachedState]
+    nextStates (dynState, dirs) = let
+      state = stateOf dynState
+      Just upState = act (Move MUp) state
+      Just downState = act (Move MDown) state
+      Just leftState = act (Move MLeft) state
+      Just rightState = act (Move MRight) state
+      in [(dynStateOf upState, MUp : dirs),
+          (dynStateOf downState, MDown : dirs),
+          (dynStateOf leftState, MLeft : dirs),
+          (dynStateOf rightState, MRight : dirs)]
+
+    -- remove states which appear elsewhere in the list
+    -- this does not preserve order
+    deduplicateStates :: [ReachedState] -> [ReachedState]
+    deduplicateStates = foldl (\acc state -> if fst state `elem` map fst acc then acc else state : acc) []
+
+    -- given cached states and states needing to be checked,
+    -- make new cached states and states needing to be checked
+    newCachedStates :: [ReachedState] -> [ReachedState] -> ([ReachedState], [ReachedState])
+    newCachedStates cached toCheck = let
+      reached = toCheck >>= nextStates
+      dedupStates = deduplicateStates reached
+      newCached = toCheck ++ cached
+      newlyReached = filter (not . (`elem` (map fst newCached)) . fst) reached
+      in (newCached, newlyReached)
+
+    -- a state formed by combining the given dynamic state with the static state of the game
+    stateOf :: DynamicState -> GameState
+    stateOf (playerLocation, boxLocations) =
+      startingState { playerLocation = playerLocation, boxLocations = boxLocations }
+
+    -- takes cached states and states to check, returning moves of the shortest solution
+    solve' :: [ReachedState] -> [ReachedState] -> [MoveDirection]
+    solve' cached toCheck = case filter (won . stateOf . fst) toCheck of
+      [] -> let (newCached, newToCheck) = newCachedStates cached toCheck
+            in solve' newCached newToCheck
+      ((_, soln) : _) -> soln
